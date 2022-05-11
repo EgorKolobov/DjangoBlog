63d1c35997653ead2d319f9d659fdf1b54ec3652
Egor Kolobov <ekoloboff@gmail.com>
2022-05-11 22:49:15 +0300

    Updated settings

--- Django_practice/settings.py
+++ Django_practice/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9*gpinp81n3t+71l=@-7s-_x5p&624n7$4c6=46%z!q$m$w-=j'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True#str(os.environ.get('DEBUG')) == '1'
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost']
+ALLOWED_HOSTS = ['127.0.0.1','localhost', 'egors-djangoblog.herokuapp.com']
 
 # Application definition
 
